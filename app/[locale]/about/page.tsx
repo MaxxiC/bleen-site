b16738cffabb731772cb527a85642d5253b20166
MaxxiC <andmassic@icloud.com>
2025-09-27 02:09:08 +0200

    esemple init for lang usage

---
+++ app/[locale]/about/page.tsx
@@ -0,0 +1,13 @@
+// File: app/[locale]/about/page.tsx
+import { useTranslations } from 'next-intl';
+
+export default function AboutPage() {
+  const t = useTranslations('About');
+
+  return (
+    <div>
+      <h1>{t('title')}</h1>
+      <p>{t('description')}</p>
+    </div>
+  );
+}
