a74ee7a424f08ff14aefd9b0dce2981d8333bbb4
MaxxiC <andmassic@icloud.com>
2025-09-27 02:01:44 +0200

    init middleware and file lang it

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+// File: middleware.ts
+import createMiddleware from "next-intl/middleware";
+
+export default createMiddleware({
+  locales: ["en", "it"],
+  defaultLocale: "it",
+});
+
+export const config = {
+  matcher: ["/((?!api|_next|.*\\..*).*)"],
+};
